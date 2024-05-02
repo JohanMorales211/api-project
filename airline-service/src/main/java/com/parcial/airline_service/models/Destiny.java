a84356db8650811c443f09525f72d97f156fa1ee
santiagobeltranf <santiago.beltran.7813@eam.edu.co>
2024-05-02 16:42:46 -0500

    Subiendo Delelte origin y destiny

--- airline-service/src/main/java/com/parcial/airline_service/models/Destiny.java
+++ airline-service/src/main/java/com/parcial/airline_service/models/Destiny.java
@@ -18,7 +18,7 @@ public class Destiny implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Long id;
 
     @Column(nullable = false)
     private String name;
--- airline-service/src/main/java/com/parcial/airline_service/models/Flight.java
+++ airline-service/src/main/java/com/parcial/airline_service/models/Flight.java
@@ -20,7 +20,7 @@ public class Flight implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Long id;
 
     @Column(nullable = false)
     private String airline;
